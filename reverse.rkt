1a86682f746fb986f49a93a98222de01db65a660
Saltb0rn <asche34@outlook.com>
2018-07-13 17:40:54 +0800

    Add reverse

--- reverse.rkt
+++ reverse.rkt
@@ -1,5 +1,14 @@
 #lang racket
-
+(require syntax/strip-context)
+(provide (except-out (all-from-out racket) #%module-begin))
 (define (read-syntax path port)
-  (void))
-(provide read-syntax)
+  (define strsrc (string-trim (port->string port)))
+  (define charlst (string->list strsrc))
+  (define intlst (map char->integer charlst))
+  (strip-context
+   #`(module i2c-reverse-mod i2c/reverse #,@intlst)))
+
+(module+ reader
+  (provide read-syntax))
+
+(provide #%module-begin)
